7d98d7274b2ea9092392424522ad93cf8b2e00a5
Jorge Vinaches <jorge.vinaches@gruposantander.com>
2018-10-27 20:48:06 +0200

    Create project

---
+++ ThePersonalTrainerClub/ThePersonalTrainerClub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThePersonalTrainerClub
+//
+//  Created by VINACHES LOPEZ JORGE on 27/10/2018.
+//  Copyright © 2018 eXceptionCoders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
